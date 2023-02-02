dd01f685fefed69f27e2721cc3e41233501f9c70
the4nna <the4nna@outlook.com>
2023-02-02 14:18:51 -0300

    estilizando footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,19 +4,23 @@ import { Container } from "./styles";
 const Footer = () => {
   return (
     <Container>
-      Desenvolvido por:
-      <DevCard
-        name="The4nna"
-        description="Front end developer e UX/UI designer"
-        github="https://github.com/the4nna/"
-        linkedin="https://www.linkedin.com/in/anna-carolina-sousa/"
-      />
-      <DevCard
-        name="Erivan"
-        description="Full stack developer"
-        github="https://github.com/erivanf/"
-        linkedin="https://www.linkedin.com/in/erivan-ferreira-dev/"
-      />
+      <h4> Desenvolvido por: </h4>
+      <div className="container-the4nna">
+        <DevCard
+          name="The4nna"
+          description="Front end developer e UX/UI designer"
+          github="https://github.com/the4nna/"
+          linkedin="https://www.linkedin.com/in/anna-carolina-sousa/"
+        />
+      </div>
+      <div className="container-erivan">
+        <DevCard
+          name="Erivan"
+          description="Full stack developer"
+          github="https://github.com/erivanf/"
+          linkedin="https://www.linkedin.com/in/erivan-ferreira-dev/"
+        />
+      </div>
     </Container>
   );
 };
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,3 +1,26 @@
 import styled from "styled-components";
 
-export const Container = styled.footer``;
+export const Container = styled.footer`
+    font-size: 10px;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    background: black;
+    height: 11vh;
+
+    h4 {
+        font-size: 12px;
+    }
+
+    .container-the4nna {
+        float: left;
+        width: 50%;
+        margin-top: -30px;
+    }
+
+    .container-erivan {
+        float: right;
+        width: 50%;
+        margin-top: -30px;
+    }
+`;
